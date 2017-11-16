3647f2b63f203a3d2c0f75a0c8ecfcbfb78d7fe2
Paduu <patrick.ramseier@gmail.com>
2017-11-16 10:48:23 +0000

    configured statis files

--- event_voteings/settings.py
+++ event_voteings/settings.py
@@ -137,6 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = "/home/polygon/event_voteings/static/"
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
